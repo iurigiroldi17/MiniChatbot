d71f9915e7e9f5150197abb58565ca920b4dce6d
felipest04 <santosofeliipe@gmail.com>
2024-10-23 20:11:55 -0300

    primeiro commit

--- src/MiniChatbot.java
+++ src/MiniChatbot.java
@@ -3,16 +3,16 @@ public class MiniChatbot {
     public String responder(String usuarioInput) {
 
         if (usuarioInput.contains("sair")) {
-            return "Até mais! Espero ter ajudado.";
+            return "Ate mais! Espero ter ajudado.";
 
-        } else if (usuarioInput.contains("olá") || usuarioInput.contains("oi")) {
-            return "Olá! Como posso ajudar você hoje?";
+        } else if (usuarioInput.contains("ola") || usuarioInput.contains("oi")) {
+            return "Olá! Como posso te ajudar?";
 
         } else if (usuarioInput.contains("qual é o seu nome")) {
-            return "Meu nome é MiniChatbot.";
+            return "Meu nome é MiniChatbot. Mas para os mais próximos é Botzin";
 
-        } else if (usuarioInput.contains("como está você")) {
-            return "Estou funcionando em perfeitas condiçoes, obrigado.";
+        } else if (usuarioInput.contains("como esta voce")) {
+            return "Estou bem demais, obrigado.";
         } else {
 
             return "Desculpe, eu não entendi sua pergunta. Pode reformular?";

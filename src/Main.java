15d20bce8616fe796ddf44a35262fd2cf0d344fe
felipest04 <santosofeliipe@gmail.com>
2024-10-23 20:06:36 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MiniChatbot chatbot = new MiniChatbot();
+        System.out.println("Olá! Como posso ajudar você hoje?");
 
+        while (true) {
+            System.out.print("Você: ");
+            String usuarioInput = scanner.nextLine();
+
+            String resposta = chatbot.responder(usuarioInput);
+            System.out.println("MiniChatbot: " + resposta);
+
+            if (usuarioInput.contains("sair")) {
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
--- src/MiniChatbot.java
+++ src/MiniChatbot.java
@@ -1,40 +1,17 @@
-import java.util.Scanner;
-
 public class MiniChatbot {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Olá! Como posso ajudar você hoje?");
-
-        while (true) {
-            System.out.print("Você: ");
-            String usuarioInput = scanner.nextLine();
-
-            if (usuarioInput.contains("sair")) {
-
-
-                System.out.println("MiniChatbot: Até mais! Espero ter ajudado.");
-                break;
-
-            } else if (usuarioInput.contains("olá") || usuarioInput.contains("oi")) {
-
-                System.out.println("MiniChatbot: Olá! Como posso ajudar você hoje?");
-
-            } else if (usuarioInput.contains("qual é o seu nome?")) {
-
-
-                System.out.println("MiniChatbot: Meu nome é MiniChatbot.");
-
-            } else if (usuarioInput.contains("como está você?")) {
-
-                System.out.println("MiniChatbot: Estou funcionando perfeitamente, obrigado");
-
-            } else {
-
-                System.out.println("MiniChatbot: Desculpe, eu não entendi sua pergunta. Pode reformular?");
-
-            }}
-        scanner.close();
+    public String responder(String usuarioInput) {
+        if (usuarioInput.contains("sair")) {
+            return "Até mais! Espero ter ajudado.";
+        } else if (usuarioInput.contains("olá") || usuarioInput.contains("oi")) {
+            return "Olá! Como posso ajudar você hoje?";
+        } else if (usuarioInput.contains("qual é o seu nome")) {
+            return "Meu nome é MiniChatbot.";
+        } else if (usuarioInput.contains("como está você")) {
+            return "Estou funcionando perfeitamente, obrigado.";
+        } else {
+            return "Desculpe, eu não entendi sua pergunta. Pode reformular?";
+        }
     }
 }
+
 
